d0e410a2436b8f1128f1b8a2376557f24f995cb2
Mike Miller <mike@mikemiller.uk>
2017-07-30 18:41:00 +0000

    Initial

---
+++ config/pkeys.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 29/07/17
+ * Time: 22:05
+ *
+ * Pkeys Schema
+ */
+
+return [
+    /*
+     * This key must be present
+     */
+    'schema'=>[
+        'redis'=>[
+            'tester'=>'a:test:{param?}:here'
+        ],
+        'cache'=>[
+
+        ],
+        'channels'=>[
+
+        ],
+        'events'=>[
+
+        ]
+    ],
+    /*
+     * Optional delimiters
+     */
+    //'delimiters=>[':','~','-,'.']
+];
